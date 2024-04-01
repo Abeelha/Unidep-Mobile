960427907d1f222b2efeae50de0b628fe9ea724f
Theodoro Bertol <dev.theodoro@gmail.com>
2024-04-01 20:05:52 -0300

    pronto

--- main/Trabalho_Faturas/main.dart
+++ main/Trabalho_Faturas/main.dart
@@ -1,46 +1,44 @@
-import 'dart:io';
+import 'dart:io'; // importa o faz tudo
 
-void main() async {
-  final List<String> fileNames = ['arquivo1.txt', 'arquivo2.txt'];
-  List<Map<String, dynamic>> financialData = [];
+//Adriel Souza
+//Theo Bertol
 
-  // Leitura dos dados dos arquivos
-  for (var fileName in fileNames) {
-    final file = File(fileName);
-    List<String> lines = await file.readAsLines();
-    // Ignora o cabeçalho
-    lines.removeAt(0);
-    lines.forEach((line) {
-      final data = line.split(', ');
-      financialData.add({
-        'Data': data[0],
-        'Receitas': double.parse(data[1]),
-        'Despesas': double.parse(data[2]),
-      });
-    });
+Future<void> main() async {
+  List<String> files = ['arquivo1.txt', 'arquivo2.txt']; // pegando arquivos
+  double totalReceitas = 0.0, totalDespesas = 0.0; //inicizando variaveis
+
+  for (String file in files) {
+    List<String> lines =
+        await File(file).readAsLines(); // le o arquivo linha por linha
+
+    for (var i = 1; i < lines.length; i++) {
+      var parts = lines[i].split(
+          ', '); // ignora a primeira linha do cabeçalho assim como minha ex me ignorava :)
+
+      totalReceitas +=
+          double.parse(parts[1]); // convertendo e somando os valores
+      totalDespesas +=
+          double.parse(parts[2]); // convertendo e somando os valores
+    }
   }
 
-  // Cálculo dos totais
-  double totalReceitas = financialData.fold(
-      0, (previousValue, element) => previousValue + element['Receitas']);
-  double totalDespesas = financialData.fold(
-      0, (previousValue, element) => previousValue + element['Despesas']);
-  double lucroTotal = totalReceitas - totalDespesas;
+  double lucroTotal = totalReceitas - totalDespesas; //soma as parada
 
-  // Geração do relatório
-  String report = '''
+  ///gera relatorio
+  String relatorio = '''
 Relatório Financeiro da Empresa
 
-Período Analisado: ${financialData.first['Data']} - ${financialData.last['Data']}
+Período Analisado: ${files.first.split('.').first} - ${files.last.split('.').first}
 
 Total de Receitas: R\$ ${totalReceitas.toStringAsFixed(2)}
 Total de Despesas: R\$ ${totalDespesas.toStringAsFixed(2)}
 Lucro Total: R\$ ${lucroTotal.toStringAsFixed(2)}
 ''';
 
-  // Escrita do relatório em um novo arquivo
-  final reportFile = File('relatorio.txt');
-  await reportFile.writeAsString(report);
+  // a porra da IDE ta salvando e identando codigo abaixo tudo errado... mas deixa assim
 
-  print('Relatório gerado com sucesso!');
+  await File('relatorio.txt')
+      .writeAsString(relatorio); // taca o relatorio em strring
+  print(
+      'Relatório gerado com sucesso.'); // parabens, nao deu erro no codigo, boa noite vou dormir
 }
